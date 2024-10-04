3d45867fc20b7b9d19fc579a58d68338940b30a6
21-abhishekyadav <21abhishek.ay@gmail.com>
2024-10-04 10:55:12 +0530

    saved url in mongodb

---
+++ data.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const Urlschema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    }
+})
+const Url = mongoose.model('Url', Urlschema);
+module.exports = Url;
--- index.js
+++ index.js
@@ -1,9 +1,41 @@
-const express = require('express')
-const app = express()
-const port = 4000
+const connectToMongo=require('./db');
+const express = require('express');
+const Url = require("./data");
+var cors = require('cors')
 
-app.get("/",(req,res)=>{
-    res.send("express app")
+
+connectToMongo();
+
+const app = express();
+const port = 4000
+app.use(cors());
+app.use(express.json());;
+
+app.post("/",(req,res)=>{
+   
+    async function saveurl() {
+
+        //to create ans save hash of a pswd instead of plain text
+        // const salt = await bcrypt.genSalt(10);
+        // const secPass = await bcrypt.hash(req.body.password, salt);
+
+
+        const url = new Url({
+            name : req.body.name,
+            
+        });
+        try {
+            const Url = await url.save();
+            const data = ({ url: { urlname: url.name } });
+            res.json({ data });
+        }
+        catch (err) {
+            res.status(500).json({error: "Internal server error occured"});
+        }
+    }
+    saveurl();
+
+
 });
 
 app.listen(port, () => {
